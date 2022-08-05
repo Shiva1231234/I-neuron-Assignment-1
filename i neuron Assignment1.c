0626b1f5f70daecaebcf13ecb3531f429a7153ab
Shiva1231234 <109754082+Shiva1231234@users.noreply.github.com>
2022-08-05 21:44:28 +0530

    Add files via upload

---
+++ i neuron Assignment1.c
@@ -0,0 +1,117 @@
+ASSIGNMENT = 1(I NEURON)
+Question = 1
+/*
+#include<stdio.h>
+int main()
+{
+printf("hello");
+}
+*\
+Question = 2
+/*
+#include <stdio.h>
+
+int main()
+{
+printf("hello\n");
+printf("student");
+
+    return 0;
+}
+*\
+Question = 3
+/*
+#include<stdio.h>
+
+int main()
+{
+    printf("\"my sirg\"");
+
+    return 0;
+}
+*\
+Question = 4
+/*
+#include <stdio.h>
+int main()
+{
+  float Pi=3.14, area, circumference, radius;
+
+  printf("Enter radius of circle: ");
+
+  scanf("%f",&radius);
+  area = Pi * radius * radius;
+  printf("Area of circle is: %f\n",area);
+
+
+  circumference = 2 * Pi * radius;
+  printf("Circumference of circle is: %f",circumference);
+
+  return 0;
+}
+*\
+Question = 5
+/*
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  char a[100];
+  int length;
+
+  printf("Enter a string to calculate its length\n");
+  gets(a);
+
+  length = strlen(a);
+
+  printf("Length of the string = %d\n", length);
+
+  return 0;
+}
+*\
+Question = 6
+/*
+#include <stdio.h>
+
+int main() {
+    printf("\"hello,Amit kumar\"");
+
+
+    return 0;
+}
+*\
+Question = 7
+/*
+#include <stdio.h>
+
+int main() {
+    printf("%%d");
+
+
+    return 0;
+}
+*\
+Question = 8
+/*
+#include <stdio.h>
+
+int main()
+{
+    printf("\\n");
+
+
+    return 0;
+}
+*\
+Question = 9
+/*
+#include <stdio.h>
+
+int main() {
+    printf("\\\\");
+
+
+    return 0;
+}
+*\
+
